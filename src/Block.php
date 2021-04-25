90d7402025d9e1d3b981bc14e49e8f301f1ae0ee
Jordan Pakrosnis <jpak@jordanpak.com>
2021-04-25 10:29:58 -0400

    Init block handling classes

---
+++ src/Block.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Abstraction of a block
+ *
+ * @since   1.0.0
+ * @package AJAX_Posts_Block
+ */
+
+namespace AJAX_Posts_Block;
+
+use function AJAX_Posts_Block\get_block_template;
+
+// exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Base class for registering and handling a block
+ *
+ * @since 1.0.0
+ */
+class Block {
+
+	/**
+	 * Internal block name
+	 *
+	 * @since 1.0.0
+	 * @var   string
+	 */
+	protected $name;
+
+	/**
+	 * Does the block render as a template?
+	 *
+	 * @since 1.0.0
+	 * @var   boolean
+	 */
+	protected $templated = false;
+
+	/**
+	 * Set everything up
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $name  Block name.
+	 */
+	public function __construct( $name = null ) {
+		$this->name = $name ?: $this->name;
+
+		add_action( 'init', [ $this, 'do_registration' ] );
+	}
+
+	/**
+	 * Register the block with WordPress
+	 *
+	 * @since 1.0.0
+	 */
+	public function do_registration() {
+
+		register_block_type(
+			"full-score-events/{$this->name}",
+			array_filter(
+				[
+					'editor_script'   => Blocks::EDITOR_ASSET_HANDLE,
+					'editor_style'    => Blocks::EDITOR_ASSET_HANDLE,
+					'style'           => Blocks::ASSET_HANDLE,
+					'attributes'      => $this->get_attributes(),
+					'render_callback' => $this->templated ? [ $this, 'render' ] : null,
+				]
+			)
+		);
+
+		$this->do_meta_registration();
+	}
+
+	/**
+	 * Get block's attributes
+	 *
+	 * @since  1.0.0
+	 *
+	 * @return array
+	 */
+	protected function get_attributes() {
+		return [];
+	}
+
+	/**
+	 * Do extra meta post meta registration for attribute sourcing
+	 *
+	 * @since 1.0.0
+	 */
+	protected function do_meta_registration() {
+	}
+
+	/**
+	 * Render the block
+	 *
+	 * Only used if templated.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param  array  $attrs   Block's attributes.
+	 * @param  string $content Block's contents (InnerBlocks).
+	 * @return string          Block HTML.
+	 */
+	public function render( $attrs, $content ) {
+
+		if ( $content ) {
+			$attrs['content'] = $content;
+		}
+
+		return get_block_template( $this->name, $attrs );
+	}
+}
---
+++ src/Blocks.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Editor blocks handler
+ *
+ * @since   1.0.0
+ * @package AJAX_Posts_Block
+ */
+
+namespace AJAX_Posts_Block;
+
+// exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Block registration and asset handling
+ *
+ * @since 1.0.0
+ */
+class Blocks {
+
+	/**
+	 * Block editor CSS/JS asset handle
+	 *
+	 * @since 1.0.0
+	 * @var   string
+	 */
+	const EDITOR_ASSET_HANDLE = 'ajax-posts-block-editor';
+
+	/**
+	 * Block front end + editor CSS handle
+	 *
+	 * @since 1.0.0
+	 * @var   string
+	 */
+	const ASSET_HANDLE = 'ajax-posts-block-blocks';
+
+	/**
+	 * Spin everything up
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct() {
+		add_action( 'init', [ __CLASS__, 'do_asset_registration' ] );
+		add_action( 'enqueue_block_editor_assets', [ __CLASS__, 'enqueue_editor_assets' ] );
+
+		new Block( 'posts' );
+	}
+
+	/**
+	 * Register assets for blocks/editor to use
+	 *
+	 * @since 1.0.0
+	 */
+	public static function do_asset_registration() {
+		$build_dir = AJAX_POSTS_BLOCK_DIR . 'build';
+		$build_url = AJAX_POSTS_BLOCK_URL . 'build';
+		$asset     = require "$build_dir/editor.asset.php";
+
+		// Register block JS.
+		wp_register_script(
+			self::EDITOR_ASSET_HANDLE,
+			"$build_url/editor.js",
+			$asset['dependencies'],
+			$asset['version'],
+			true
+		);
+
+		// Register editor-specific block styles.
+		wp_register_style(
+			self::EDITOR_ASSET_HANDLE,
+			"$build_url/editor.css",
+			[],
+			filemtime( "$build_dir/editor.css" )
+		);
+
+		// Register front-end + editor block styles.
+		wp_register_style(
+			self::ASSET_HANDLE,
+			"$build_url/style-editor.css",
+			[],
+			filemtime( "$build_dir/style-editor.css" )
+		);
+	}
+
+	/**
+	 * Enqueue editor assets
+	 *
+	 * @since 1.0.0
+	 */
+	public static function enqueue_editor_assets() {
+
+		// Always enqueue editor script/styles since sidebar plugins aren't
+		// registered in PHP.
+		wp_enqueue_script( self::EDITOR_ASSET_HANDLE );
+		wp_enqueue_style( self::EDITOR_ASSET_HANDLE );
+	}
+}
