7e421ded6fb7d1af7d9ae1147266ccb1812736c2
Jordan Pakrosnis <jpak@jordanpak.com>
2021-04-25 10:31:13 -0400

    Init editor/public styling

---
+++ src/editor/index.js
@@ -0,0 +1,9 @@
+/**
+ * Block editor blocks + helpers
+ *
+ * @since 1.0.0
+ */
+
+// global helpers
+import './style.scss';
+import './index.scss';
---
+++ src/public/index.js
@@ -0,0 +1,7 @@
+/**
+ * Front end-only CSS and JS
+ *
+ * @since 1.0.0
+ */
+
+import './global.scss';
