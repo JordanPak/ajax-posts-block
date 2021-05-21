795927d6c4e7c119412b21cab899fd7319332814
Jordan Pakrosnis <jpak@jordanpak.com>
2021-05-21 15:15:24 -0400

    Add Blocks' asset registration test

--- tests/Test_Blocks.php
+++ tests/Test_Blocks.php
@@ -44,4 +44,47 @@ class Test_Blocks extends WP_UnitTestCase {
 		$registry = WP_Block_Type_Registry::get_instance();
 		$this->assertTrue( $registry->is_registered( 'ajax-posts-block/posts' ) );
 	}
+
+
+	/**
+	 * Test do_asset_registration()
+	 *
+	 * @covers AJAX_Posts_Block\Blocks::do_asset_registration()
+	 */
+	public function test_do_asset_registration() {
+		/*
+		 * Check script registration and test some versions/deps.
+		 */
+		$scripts = wp_scripts();
+
+		$editor_script = $scripts->registered['ajax-posts-block-editor'];
+		$this->assertEquals( 'ajax-posts-block-editor', $editor_script->handle );
+		$this->assertContains( 'wp-url', $editor_script->deps );
+		$this->assertNotEmpty( $editor_script->ver );
+
+		$public_script = $scripts->registered['ajax-posts-block-blocks'];
+		$this->assertEquals( 'ajax-posts-block-blocks', $public_script->handle );
+		$this->assertContains( 'wp-api-fetch', $public_script->deps );
+		$this->assertNotEmpty( $public_script->ver );
+
+		// Validate inclusion of post type(s) in localized variable.
+		$localized_public_json = trim( str_replace( 'var apbHelper = ', '', $public_script->extra['data'] ), ';' );
+		$localized_public_json = json_decode( $localized_public_json );
+		$this->assertObjectHasAttribute( 'post', $localized_public_json->types );
+
+		/*
+		 * Check style registration and test some versions/deps.
+		 */
+		$styles = wp_styles();
+
+		$editor_style = $styles->registered['ajax-posts-block-editor'];
+		$this->assertEquals( 'ajax-posts-block-editor', $editor_style->handle );
+		$this->assertEmpty( $editor_style->deps );
+		$this->assertNotEmpty( $editor_style->ver );
+
+		$public_style = $styles->registered['ajax-posts-block-blocks'];
+		$this->assertEquals( 'ajax-posts-block-blocks', $public_style->handle );
+		$this->assertEmpty( $public_style->deps );
+		$this->assertNotEmpty( $public_style->ver );
+	}
 }
