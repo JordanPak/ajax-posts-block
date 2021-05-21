5c3e47bdc3db10b7606ff70050304099df9b2426
Jordan Pakrosnis <jpak@jordanpak.com>
2021-05-21 14:22:05 -0400

    Add "posts" block registration test to Test_Blocks' constructor

--- tests/Test_Blocks.php
+++ tests/Test_Blocks.php
@@ -35,7 +35,13 @@ class Test_Blocks extends WP_UnitTestCase {
 	 * @covers AJAX_Posts_Block\Blocks::__construct()
 	 */
 	public function test_construct() {
+
+		// Check hooks.
 		$this->assertEquals( 10, has_action( 'init', [ $this->instance, 'do_asset_registration' ] ) );
 		$this->assertEquals( 10, has_action( 'enqueue_block_editor_assets', [ $this->instance, 'enqueue_editor_assets' ] ) );
+
+		// Make sure "Posts" block is registered.
+		$registry = WP_Block_Type_Registry::get_instance();
+		$this->assertTrue( $registry->is_registered( 'ajax-posts-block/posts' ) );
 	}
 }
