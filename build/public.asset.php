e267def27bb20745b77978868bd6dfd27c354e59
Jordan Pakrosnis <jpak@jordanpak.com>
2021-05-25 21:12:49 -0400

    Update built assets for last several commits

--- build/public.asset.php
+++ build/public.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'b0919d47edcdc0e34fee0549fb128b21');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'e93a45365f3e1005903d3b0593ee1c01');
